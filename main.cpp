a75d1fa04100e98311f5453f40b4012455cffee4
Abramenko Kirill <p.acc.ksa@gmail.com>
2024-04-20 14:46:39 +0300

    record class added

--- main.cpp
+++ main.cpp
@@ -1,4 +1,27 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+#include <map> 
+
+struct Date {
+    int day;
+    int month;
+    int year;
+};
+
+struct Record {
+    std::string full_name; // Ф.И.О. студента
+    Date birth_date; // дата рождения
+    int admission_year; // год поступления в институт
+    std::string faculty; // факультет (институт)
+    std::string department; // кафедра
+    std::string group; // группа
+    std::string record_book_number; // номер зачетной книжки
+    std::pair<std::string, std::vector<int>> subjects_and_marks; // названия предметов и оценки по каждому предмету в каждой сессии
+    std::string assignment; // конкретное задание, вариант которого определяется 2-мя последними цифрами зачетной книжки (шифр)
+};
+
 
 int main (int argc, char **argv){
     std::cout << "Hello world!" << std::endl;
